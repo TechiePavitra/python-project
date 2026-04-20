34565398866e917d2947668a82cc4503fe82690e
TechiePavitra <techiepavitra@gmail.com>
2026-04-20 13:28:12 +0530

    Jupyter Notes has been added (after  #a4ab697)

---
+++ Checking-for-Special-Items.py
@@ -0,0 +1,5 @@
+requested_topings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_toping in requested_topings:
+    print(f"Your Requested Toping: {requested_toping} has been added")
+
+print("Finished Making Pizza!")
