01270d6de69fd7a590fa49259b57c3ffcec76524
TechiePavitra <techiepavitra@gmail.com>
2026-04-24 13:44:06 +0530

    Update try_it_yourself.py

--- random_exercises/failed attempts or random exercises/try_it_yourself.py
+++ random_exercises/failed attempts or random exercises/try_it_yourself.py
@@ -1,20 +1,23 @@
 print("The Python Crash Course 3rd Edition is very great book!")
 
-test="This information is now been updated!"
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
-print(test)
+# Bad Habit
+# test="This information is now been updated!"
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
+# print(test)
 
 
-
+# Good habit use this code instead! 
+for i in range(1,15):
+    print("This information is now been updated!")
