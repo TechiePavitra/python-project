eaf5452a586432100f7c384f847fbea17b7ed324
TechiePavitra <techiepavitra@gmail.com>
2026-05-18 21:20:55 +0530

    Create 7.7-Infinity.py

---
+++ notes_and_solutions/python_crash_course_exercises/7.7-Infinity.py
@@ -0,0 +1,9 @@
+# Write a loop that never ends, and run it. 
+# (To end the loop, press CTRL-C or close the window displaying the output.)
+
+# counts from 1 to Infinity
+current_value = 1
+
+while True:
+    print(current_value)
+    current_value += 1 
