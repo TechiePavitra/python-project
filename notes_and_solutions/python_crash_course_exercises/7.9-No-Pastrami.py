715b3cd0fa38d42dcf805c932e5b982b1fab1ecb
TechiePavitra <techiepavitra@gmail.com>
2026-05-20 17:05:56 +0530

    Add solution for Exercise 7.9: No Pastrami
    Add a Python script solving Exercise 7-9: initializes sandwich_orders with multiple 'pastrami' entries, prints a notice that the deli ran out of pastrami, removes all 'pastrami' occurrences with a while loop, moves remaining orders to finished_sandwiches, and prints the sandwiches that were made.

---
+++ notes_and_solutions/python_crash_course_exercises/7.9-No-Pastrami.py
@@ -0,0 +1,19 @@
+# Using the list sandwich_orders from Exercise 7-8, make sure the sandwich 'pastrami' appears in the list at least three times. 
+# Add code near the beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all occurrences of 'pastrami' from sandwich_orders.
+# Make sure no pastrami sandwiches end up in finished_sandwiches.
+
+sandwich_orders = ["pastrami","grilled cheese sandwich", "pastrami", "tuna sandwich", "club sandwich", "pastrami", "sloopy joe sandwich", "pastrami"]
+print(f"The deli has run out of pastrami stock")
+finished_sandwiches = []
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove("pastrami")
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I made {current_sandwich}.")
+    finished_sandwiches.append(current_sandwich)
+    
+print(f"\n--- Sandwiches that were made ---")
+for sandwich in finished_sandwiches:
+    print(sandwich.title())
