403b1f6ffbd8dfedadff12d54339823675126dc7
TechiePavitra <techiepavitra@gmail.com>
2026-05-23 17:02:23 +0530

    Refactor generator into parameterized function
    Replace the procedural script that built separate even/odd lists with a single parameterized function number_generator(number, limit, number_type). The function prints numbers of type 'even' or 'odd' in the given range and includes a docstring; global lists and separate print loops were removed. Updated call: number_generator(1, 100, 'odd').

--- references/projects/even_odd_number_generator/generator.py
+++ references/projects/even_odd_number_generator/generator.py
@@ -1,23 +1,13 @@
-# Change Variable "even"
-# if you want Even Numbers use "even = True"
-# if you want Odd Numbers use "even = False"
-even_numbers = []
-odd_numbers = []
+# this is a even and odd number generator function
+# use "even" or "odd" in third argument to generate even or odd numbers
 
-num = 1 
-limit = 100 # change this variable to manipulate this program.
-
-while num <= limit:
-    if num % 2 == 0:
-        even_numbers.append(num)
-    else:
-        odd_numbers.append(num)
-    num += 1
-
-print("Here are even numbers: ")
-for even_number in even_numbers:
-    print(even_number)
-
-print("\nHere are odd numbers: ")
-for odd_number in odd_numbers:
-    print(odd_number)        
+def number_generator(number, limit, number_type):
+    """Even and odd number generator."""
+    while number <= limit:
+        if number_type == 'even' and number % 2 == 0:
+            print(number)
+        elif number_type == 'odd' and number % 2 != 0:
+            print(number)
+        number += 1
+        
+number_generator(1, 100, "odd")
