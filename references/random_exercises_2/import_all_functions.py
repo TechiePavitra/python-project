7c5a5a43c310f1cda23deaf3adca53679803a35b
TechiePavitra <techiepavitra@gmail.com>
2026-05-31 18:09:28 +0530

    Import all functions from a module

---
+++ references/random_exercises_2/import_all_functions.py
@@ -0,0 +1,9 @@
+from modules.pizza import *
+# to import all functions from module use the e asterisk (*) operator.
+
+make_pizza(12, "pepproni")
+make_pizza(16, "mushrooms", "green peppers", "extra cheese")
+
+# you can call each function by name without using the dot notation.
+# However, it’s best not to use this approach when you’re working with larger modules that you didn’t write.
+# generally avoid this when you are working with large projects.
