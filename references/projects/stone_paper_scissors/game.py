444b878a98cfbd73f5847aa7e0b195df9ad95287
TechiePavitra <techiepavitra@gmail.com>
2026-06-04 20:33:34 +0530

    refactor code

--- references/projects/stone_paper_scissors/game.py
+++ references/projects/stone_paper_scissors/game.py
@@ -2,8 +2,9 @@ import random
 
 score = 0
 attempts = 0
+flag = True
 
-while attempts < 10:
+while attempts < 10 and flag:
     # Bot 
     selectors = ['stone', 'paper', 'scissors']
     bot_choice = random.choice(selectors)
@@ -12,33 +13,23 @@ while attempts < 10:
     prompt = "\nEnter your choice"
     prompt += "\nEnter Stone, Paper, or Scissors:  "
     user_choice = input(prompt)
+    user_choice = user_choice.lower()
 
     if user_choice.lower() == bot_choice:
         print(f"The computer selected {bot_choice.title()}, and you selected {user_choice.title()}. It's a tie!")
     else:
-        
-        # Lost
-        if user_choice.lower() == 'paper' and bot_choice == 'scissors': 
-            print(f"The computer selected {bot_choice.title()}. Unfortunately, you lost!")
-            break
-        elif user_choice.lower() == 'scissors' and bot_choice == 'stone':
-            print(f"The computer selected {bot_choice.title()}. Unfortunately, you lost!")
-            break
-        elif user_choice.lower() == 'stone' and bot_choice == 'paper':
-            print(f"The computer selected {bot_choice.title()}. Unfortunately, you lost!")
-            break
-    
-        # Won 
-        elif user_choice.lower() == 'scissors' and bot_choice == 'paper':
+        winning_rules = {
+            "scissors": "paper",
+            "paper": "stone",
+            "stone": "scissors",
+        }
+
+        if bot_choice == winning_rules[user_choice]:
             print(f"The computer selected {bot_choice.title()}. Congratulations, you won!")
             score += 1000
-        elif user_choice.lower() == 'paper' and bot_choice == 'stone':
-            print(f"The computer selected {bot_choice.title()}. Congratulations, you won!")
-            score += 1000
-        elif user_choice.lower() == 'stone' and bot_choice == 'scissors':
-            print(f"The computer selected {bot_choice.title()}. Congratulations, you won!")
-            score += 1000
-    
+        else:
+            print(f"The computer selected {bot_choice.title()}. Unfortunately, you lost!")
+            flag = False  
     attempts += 1
 
 print(f"Your total score is: ${score}")
