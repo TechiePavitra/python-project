0b76d1803dc40245fae74bb8021bde7036186722
TechiePavitra <techiepavitra@gmail.com>
2026-06-05 22:02:45 +0530

    Small changes

--- references/random_exercises_2/default_attributes_class.py
+++ references/random_exercises_2/default_attributes_class.py
@@ -7,7 +7,7 @@ class Car:
         self.model = model
         self.year = year
         self.color = color 
-        self.odometer = 0 # this is default attribute
+        self.odometer_reading = 0 # this is default attribute
     
     # Methods
     def get_descriptive_name(self):
@@ -17,7 +17,7 @@ class Car:
     
     def read_odometer(self):
         """Prints a statement showing car's mileage."""
-        print(f"This car has {self.odometer} miles on it.")
+        print(f"This car has {self.odometer_reading} miles on it.")
 
 # Why we used default attribute? and what it does?
 # First look, some inputs are not possible that a user can provide,
@@ -26,7 +26,9 @@ class Car:
 # By default when we buy a car the odometer is 0, because its has not travelled to any where yet,
 # In this situation we need to an default attribute to store car's odometer value,
 # Look at the upper code we have provided an default value, and than created a new method called read_odometer,
-# Which prints "How much miles does car travelled"
+# Which prints "How much miles does car travelled",
+# Not many cars are sold with exactly 0 miles on the odometer, 
+# so we need a way to change the value of this attribute.
 
 # Let's Create a Instance
 my_car = Car("lamborghini", "sian", 2022, "red")
