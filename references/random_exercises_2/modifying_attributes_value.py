dafdc94e8eebcd2ebf5695641e1639ca4c64d9ae
TechiePavitra <techiepavitra@gmail.com>
2026-06-06 21:33:05 +0530

    Add Increment attribute's value example

--- references/random_exercises_2/modifying_attributes_value.py
+++ references/random_exercises_2/modifying_attributes_value.py
@@ -65,3 +65,48 @@ my_car = Car("lamborghini", "sian", 2022, "red")
 # Look here we are updating odometer
 my_car.update_odometer(4506)
 my_car.read_odometer()
+
+# 3rd: Incrementation
+class Car:
+    """A Simple attempt to represent car."""
+
+    # Attributes / constructor
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color 
+        self.odometer_reading = 0 # this is default attribute
+    
+    # Methods
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Prints a statement showing car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+my_car = Car("lamborghini", "sian", 2022, "red")
+
+# Look here we are updating odometer
+my_car.update_odometer(4506)
+my_car.read_odometer()
+
+# Increment certain amount
+my_car.increment_odometer(12000)
+my_car.read_odometer()
